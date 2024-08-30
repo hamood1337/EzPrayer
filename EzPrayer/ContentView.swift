da1f9a3ccbc0fd261183ef79eb13ded0164d6cbb
Ahmad Maharmeh <maharmehllc@proton.me>
2024-08-31 02:45:34 +0400

    Initial Commit

---
+++ EzPrayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EzPrayer
+//
+//  Created by Ahmad Maharmeh on 31/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EzPrayer/EzPrayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EzPrayerApp.swift
+//  EzPrayer
+//
+//  Created by Ahmad Maharmeh on 31/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct EzPrayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
